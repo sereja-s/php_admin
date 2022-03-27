f6b05acb983f4acedfe93ddc81ea95105c49937a
sereja-s <serejasuvorov1979@gmail.com>
2022-03-27 20:40:16 +0300

    10 урок

--- core/user/controller/BaseUser.php
+++ core/user/controller/BaseUser.php
@@ -6,151 +6,136 @@ use core\user\model\Model;
 
 abstract class BaseUser extends \core\base\controller\BaseController
 {
-    protected $model;
-    protected $table;
+	protected $model;
+	protected $table;
 
-    protected $set;
-    protected $menu;
+	protected $set;
+	protected $menu;
 
-    /*Проектные свойства*/
-    protected $socials;
-    /*Проектные свойства*/
+	/*Проектные свойства*/
+	protected $socials;
+	/*Проектные свойства*/
 
-    protected function inputData() {
-        $this->init();
-        !$this->model && $this->model = Model::instance();
+	protected function inputData()
+	{
+		$this->init();
+		!$this->model && $this->model = Model::instance();
 
-        $this->set = $this->model->get('settings', [
-            'order' => ['id'],
-            'limit' => 1
-        ]);
+		$this->set = $this->model->get('settings', [
+			'order' => ['id'],
+			'limit' => 1
+		]);
 
-        $this->set && $this->set = $this->set[0];
+		$this->set && $this->set = $this->set[0];
 
-        $this->menu['catalog'] = $this->model->get('catalog', [
-            'where' => ['visible' => 1, 'parent_id' => null],
-            'order' => ['menu_position']
-        ]);
+		$this->menu['catalog'] = $this->model->get('catalog', [
+			'where' => ['visible' => 1, 'parent_id' => null],
+			'order' => ['menu_position']
+		]);
 
-        $this->menu['information'] = $this->model->get('information', [
-            'where' => ['visible' => 1, 'show_top_menu' => 1],
-            'order' => ['menu_position']
-        ]);
+		$this->menu['information'] = $this->model->get('information', [
+			'where' => ['visible' => 1, 'show_top_menu' => 1],
+			'order' => ['menu_position']
+		]);
 
-        $this->socials = $this->model->get('socials', [
-            'where' => ['visible' => 1],
-            'order' => ['menu_position']
-        ]);
-    }
+		$this->socials = $this->model->get('socials', [
+			'where' => ['visible' => 1],
+			'order' => ['menu_position']
+		]);
+	}
 
-    protected function outputData() {
-        if(!$this->content) {
-            $args = func_get_arg(0);
-            $vars = $args ? $args : [];
+	protected function outputData()
+	{
+		if (!$this->content) {
 
-            //if(!$this->template) { $this->template = ADMIN_TEMPLATE . 'show'; }
+			// в переменной сохраним результат работы ф-ии php: func_get_arg()- Возвращает указанный аргумент из списка аргументов пользовательской функции (здесь- порядковый номер: 0)
+			$args = func_get_arg(0);
+			$vars = $args ? $args : [];
 
-            $this->content = $this->render($this->template, $vars);
-        }
+			//if(!$this->template) { $this->template = ADMIN_TEMPLATE . 'show'; }
 
-        $this->header = $this->render(TEMPLATE . 'include/header', $vars);
-        $this->footer = $this->render(TEMPLATE . 'include/footer', $vars);
+			$this->content = $this->render($this->template, $vars);
+		}
 
-        return $this->render(TEMPLATE . 'layout/default');
-    }
+		$this->header = $this->render(TEMPLATE . 'include/header', $vars);
+		$this->footer = $this->render(TEMPLATE . 'include/footer', $vars);
 
-    protected function img($img = '', $tag = false) {
-        if(!$img && is_dir($_SERVER['DOCUMENT_ROOT'] . PATH . UPLOAD_DIR . DEFAULT_IMAGE_DIRECTORY)) {
-            $dir = scandir($_SERVER['DOCUMENT_ROOT'] . PATH . UPLOAD_DIR . DEFAULT_IMAGE_DIRECTORY);
+		return $this->render(TEMPLATE . 'layout/default');
+	}
 
-            $imgArr = preg_grep('/' . $this->getController() .'\./i', $dir) ?: preg_grep('/default\./i', $dir);
+	protected function img($img = '', $tag = false)
+	{
+		if (!$img && is_dir($_SERVER['DOCUMENT_ROOT'] . PATH . UPLOAD_DIR . DEFAULT_IMAGE_DIRECTORY)) {
+			$dir = scandir($_SERVER['DOCUMENT_ROOT'] . PATH . UPLOAD_DIR . DEFAULT_IMAGE_DIRECTORY);
 
-            $imgArr && $img = DEFAULT_IMAGE_DIRECTORY . '/' . array_shift($imgArr);
-        }
+			$imgArr = preg_grep('/' . $this->getController() . '\./i', $dir) ?: preg_grep('/default\./i', $dir);
 
-        if($img) {
-            $path = PATH . UPLOAD_DIR . $img;
+			$imgArr && $img = DEFAULT_IMAGE_DIRECTORY . '/' . array_shift($imgArr);
+		}
 
-            if(!$tag) {
-                return $path;
-            }
+		if ($img) {
+			$path = PATH . UPLOAD_DIR . $img;
 
-            echo '<img src="'. $path .'" alt="image" title="image">';
-        }
+			if (!$tag) {
+				return $path;
+			}
 
-        return '';
-    }
+			echo '<img src="' . $path . '" alt="image" title="image">';
+		}
 
-    protected function alias($alias = '', $queryString = '') {
-        $str = '';
+		return '';
+	}
 
-        if($queryString) {
-            if(is_array($queryString)) {
-                foreach ($queryString as $key => $item) {
-                    $str .= (!$str ? '?' : '&');
+	protected function alias($alias = '', $queryString = '')
+	{
+		$str = '';
 
-                    if(is_array($item)) {
-                        $key .= '[]';
+		if ($queryString) {
+			if (is_array($queryString)) {
+				foreach ($queryString as $key => $item) {
+					$str .= (!$str ? '?' : '&');
 
-                        foreach ($item as $v) {
-                            $str .= $key . '=' . $v;
-                        }
-                    }else{
-                        $str .= $key . '=' . $item;
-                    }
-                }
-            }else{
-                if(strpos($queryString, '?') === false) {
-                    $str = '?' . $str;
-                }
+					if (is_array($item)) {
+						$key .= '[]';
 
-                $str .= $queryString;
-            }
-        }
+						foreach ($item as $v) {
+							$str .= $key . '=' . $v;
+						}
+					} else {
+						$str .= $key . '=' . $item;
+					}
+				}
+			} else {
+				if (strpos($queryString, '?') === false) {
+					$str = '?' . $str;
+				}
 
-        if(is_array($alias)) {
-            $aliasStr = '';
+				$str .= $queryString;
+			}
+		}
 
-            foreach ($alias as $key => $item) {
-                if(!is_numeric($key) && $item) {
-                    $aliasStr .= $key . '/' . $item . '/';
-                }elseif ($item) {
-                    $aliasStr .= $item . '/';
-                }
-            }
+		if (is_array($alias)) {
+			$aliasStr = '';
 
-            $alias = trim($aliasStr, '/');
-        }
+			foreach ($alias as $key => $item) {
+				if (!is_numeric($key) && $item) {
+					$aliasStr .= $key . '/' . $item . '/';
+				} elseif ($item) {
+					$aliasStr .= $item . '/';
+				}
+			}
 
-        if(!$alias || $alias === '/') {
-            return PATH . $str;
-        }
+			$alias = trim($aliasStr, '/');
+		}
 
-        if(preg_match('/^\s*https?:\/\//i', $alias)) {
-            return $alias . $str;
-        }
+		if (!$alias || $alias === '/') {
+			return PATH . $str;
+		}
 
-        return preg_replace('/\/{2,}/', '/', PATH . $alias . END_SLASH . $str);
-    }
+		if (preg_match('/^\s*https?:\/\//i', $alias)) {
+			return $alias . $str;
+		}
+
+		return preg_replace('/\/{2,}/', '/', PATH . $alias . END_SLASH . $str);
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
